418614fc93893e8a2808adb836e9c23646531383
Md Aamir Ahmed <118830391+MDAAMIRAHMED@users.noreply.github.com>
2025-07-23 07:01:24 +0000

    JS datatypes

---
+++ JS/01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 2 + 2) // we are using node.js, not browser
+
+// code readability
+
+// console.log("Hi"); console.log("Hello")
+console.log("Hi")
+console.log("Hello")
+
+// use documentation like ecma and mdn
+
+let name = "Aamir"
+let age = 15
+let isLoggedIn = false
+let setName = null
+let state;
+
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// objects
+
+console.log(typeof age)
+console.log(typeof state)
+console.log(typeof null) // object
