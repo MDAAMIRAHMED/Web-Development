ff115fe53e219402d6ae14b71ca95308bb2250f8
Md Aamir Ahmed <118830391+MDAAMIRAHMED@users.noreply.github.com>
2025-07-23 10:59:05 +0000

    JS comparisons...

---
+++ JS/01_basics/04_comparisons.js
@@ -0,0 +1,23 @@
+// console.log(1 > 2);
+// console.log(1 < 2);
+// console.log(1 >= 2);
+// console.log(1 <= 2);
+// console.log(1 == 2);
+// console.log(1 != 2);
+
+
+// console.log("2" > 1) // true
+// console.log("02" >= 2) // true
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined >= 0);
+console.log(undefined != 0) // true
+
+// ===
+console.log("2" === 2);
+
