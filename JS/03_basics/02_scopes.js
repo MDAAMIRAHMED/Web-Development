798f552c32d76e4b2c16126691ad6e6cc8787060
Md Aamir Ahmed <118830391+MDAAMIRAHMED@users.noreply.github.com>
2025-07-24 13:28:44 +0000

    JS scope level and mini hoisting...

--- JS/03_basics/02_scopes.js
+++ JS/03_basics/02_scopes.js
@@ -6,9 +6,61 @@ if (true) {
     let a = 10;
     // const b = 20;
     // var c = 30;
-    console.log("INNER: ", a)
+    // console.log("INNER: ", a)
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+// window and browser scopes are different // imp
+
+
+// ++++++++++++++++++++++++++++++++++++++
+
+let z = 10
+function one(){
+    const username = "aamir"
+
+    function two(){
+        const website = " youtube"
+        console.log(username + website);
+        
+    }
+
+    // console.log(website);
+    console.log(z)
+    
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "aamir"
+
+    if(username === "aamir"){
+        const website = " youtube"
+        console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+// +++++++++++++++++++ mini hoisting +++++++++++++++++
+
+console.log(addOne(5));
+function addOne(num) {
+    return num + 1
+}
+
+// addTwo(5)
+const addTwo = function(num) {
+    return num + 2
+}
+console.log(addTwo(5));
+ 
