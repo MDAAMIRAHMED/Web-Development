fb0ed13d93b4b2bfa0dc69d0adf2dbd2043146f2
Md Aamir Ahmed <118830391+MDAAMIRAHMED@users.noreply.github.com>
2025-07-24 16:37:01 +0000

    JS IIFE...

---
+++ JS/03_basics/04_IIFE.js
@@ -0,0 +1,20 @@
+// Immediately Invoked Function Expression - IIFE
+
+// function chai() {
+//     console.log("DB CONNECTED");
+// }
+
+// chai()
+
+(function chai() {
+    // named IIFE
+    console.log("DB CONNECTED");
+})();
+
+(() => {
+    console.log("DB CONNECTED TWO");
+})();
+
+(name => {
+    console.log(`DB CONNECTED ${name}`);
+})("aamir");
