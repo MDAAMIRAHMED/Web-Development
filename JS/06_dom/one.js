9765692e4817dd5449180046224fbbca79e0c23e
Md Aamir Ahmed <118830391+MDAAMIRAHMED@users.noreply.github.com>
2025-07-25 14:46:53 +0000

    JS dom...

--- JS/06_dom/one.js
+++ JS/06_dom/one.js
@@ -39,4 +39,36 @@ document.getElementsByClassName('heading')
 document.querySelector('#title')
 document.querySelector('.heading')
 document.querySelector('h1')
-document.querySelector('h2')
+document.querySelector('h2') // first element
+
+const myul = document.querySelector('ul')
+myul
+const turnGreen = myul.querySelector('li')
+
+turnGreen.style..backgroundColor = 'green'
+turnGreen.style.padding = '10px'
+
+turnGreen.innerText
+turnGreen.innerText = 'five'
+
+const tempLiList = document.querySelectorAll('li')
+// tempLiList -> doesn't have map like arrays but have foreach
+const myArr = [1, 2,3, 4]
+// myArr => have both map and foreach
+
+tempLiList.style.backgroundColor = 'green' // throws error
+tempLiList[0].style.backgroundColor = 'green' // because it is a nodelist
+
+const myh1 = document.querySelectorAll('h1')
+myh1.style.backgroundColor = 'green' // throws error even if it contains only one nodelist
+myh1[0].style.backgroundColor = 'green'
+
+tempLiList.forEach(function(li) {li.style.backgroundColor = 'red'})
+
+const tempClassList = document.getElementsByClassName('list-item')
+// tempClassList => HTMLCollection and doesn't have map and foreach
+
+const myConvertedArray = Array.from(tempClassList)
+myConvertedArray.forEach(li => {
+    li.style.backgroundColor = 'red'
+})
