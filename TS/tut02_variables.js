8e9005c80ef0bf6db94784839e76b5207e6979bb
Md Aamir Ahmed <118830391+MDAAMIRAHMED@users.noreply.github.com>
2025-07-29 15:56:20 +0000

    TS Number, Boolean, type inference, any...

--- TS/tut02_variables.js
+++ TS/tut02_variables.js
@@ -4,3 +4,20 @@ var greetings = "Hello Aamir";
 var num = 6;
 console.log(greetings.toLowerCase()); // suggest string methods
 console.log(num.valueOf()); // suggest number methods
+// number
+var userId = 33.44;
+userId.toFixed(1);
+// boolean
+var isLoggedIn = true;
+isLoggedIn.valueOf();
+// type inference
+var anotherNum = 6; // automatically it is a number
+anotherNum.toFixed(1); // still suggest number methods
+// but we should in this scenario
+// let hero; // any
+var hero;
+function myHero() {
+    // return true; // throws error
+    return "thor";
+}
+hero = myHero();
--- TS/tut02_variables.ts
+++ TS/tut02_variables.ts
@@ -6,4 +6,31 @@ console.log(greetings.toLowerCase()); // suggest string methods
 
 console.log(num.valueOf()); // suggest number methods
 
+// number
+
+let userId: number = 33.44;
+
+userId.toFixed(1);
+
+// boolean
+
+let isLoggedIn: boolean = true;
+isLoggedIn.valueOf();
+
+// type inference
+let anotherNum = 6; // automatically it is a number
+anotherNum.toFixed(1); // still suggest number methods
+
+// but we should in this scenario
+
+// let hero; // any
+let hero: string;
+
+function myHero() {
+    // return true; // throws error
+    return "thor";
+}
+
+hero = myHero();
+
 export {};
