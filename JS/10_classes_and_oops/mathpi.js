a0027dffc15b152843ecb368fdaed4fcf28ee18e
Md Aamir Ahmed <118830391+MDAAMIRAHMED@users.noreply.github.com>
2025-07-29 05:05:00 +0000

    JS getOwnPropertyDescriptor...

---
+++ JS/10_classes_and_oops/mathpi.js
@@ -0,0 +1,27 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math, "PI");
+
+// console.log(descriptor);
+// console.log(Math.PI);
+// Math.PI = 5;
+// console.log(Math.PI);
+
+const chai = {
+  name: "ginger tea",
+  price: 250,
+  isAvailable: true,
+
+  orderChai: function () {
+    console.log("something...");
+  },
+};
+
+Object.defineProperty(chai, "name", {
+  enumerable: false,
+});
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+for (let [key, value] of Object.entries(chai)) {
+  if (typeof value !== "function") {
+    console.log(`${key}, ${value}`);
+  }
+}
