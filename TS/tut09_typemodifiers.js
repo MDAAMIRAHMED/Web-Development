7b004c263f4d26218cd2ef51722236d0a2036f9e
Md Aamir Ahmed <118830391+MDAAMIRAHMED@users.noreply.github.com>
2025-07-31 10:06:26 +0000

    TS getters setters...

--- TS/tut09_typemodifiers.js
+++ TS/tut09_typemodifiers.js
@@ -14,15 +14,44 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // // hitesh.name // throws error because it is private property
 // hitesh.email;
 var User = /** @class */ (function () {
-    function User(email, name, userId) {
+    function User(email, name // private userId: number
+    ) {
         this.email = email;
         this.name = name;
-        this.userId = userId;
+        this._courseCount = 1;
         this.city = "Jaipur";
     }
+    User.prototype.deleteToken = function () {
+        console.log("Token Deleted");
+    };
+    Object.defineProperty(User.prototype, "getAppleEmail", {
+        get: function () {
+            return "Apple".concat(this.email);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(User.prototype, "getCourseCount", {
+        get: function () {
+            return this._courseCount;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(User.prototype, "setCourseCount", {
+        //   set setCourseCount(courseNum): string{} // set accessor has no return type
+        set: function (courseNum) {
+            if (courseNum <= 1) {
+                throw new Error("Course count should be more than 1");
+            }
+            this._courseCount = courseNum;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return User;
 }());
-var hitesh = new User("hitesh", "h@h.com", 1234);
+var hitesh = new User("hitesh", "h@h.com");
 // hitesh.city = "chennai" // throws error
 // hitesh.name // throws error because it is private property
 hitesh.email;
--- TS/tut09_typemodifiers.ts
+++ TS/tut09_typemodifiers.ts
@@ -14,16 +14,37 @@
 // hitesh.email;
 
 class User {
+  private _courseCount = 1;
   readonly city: string = "Jaipur";
   constructor(
     public email: string,
-    public name: string,
-    private userId: number
+    public name: string // private userId: number
   ) {}
+
+  private deleteToken() {
+    console.log("Token Deleted");
+  }
+  get getAppleEmail(): string {
+    return `Apple${this.email}`;
+  }
+
+  get getCourseCount(): number {
+    return this._courseCount;
+  }
+
+  //   set setCourseCount(courseNum): string{} // set accessor has no return type
+
+  set setCourseCount(courseNum: number) {
+    if (courseNum <= 1) {
+      throw new Error("Course count should be more than 1");
+    }
+    this._courseCount = courseNum;
+  }
 }
 
-const hitesh = new User("hitesh", "h@h.com", 1234);
+const hitesh = new User("hitesh", "h@h.com");
 // hitesh.city = "chennai" // throws error
 // hitesh.name // throws error because it is private property
 hitesh.email;
+// hitesh.deleteToken();// private method
 export {};
